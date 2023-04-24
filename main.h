316299a3bd071e481b7232ceca444d78a785c08b
NeneNafe <nenechiwaya@gmail.com>
2023-04-24 14:30:30 -0700

    main header file containing all prototypes and functions

--- main.h
+++ main.h
@@ -4,33 +4,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <stddef.h>
 #include <limits.h>
+#include <unistd.h>
+#include <stddef.h>
 #include <string.h>
 
 /**
- * struct format - uses conversion specifires to print a function
- * @id: conversion identfier
- * @fn: a function pointer
+ * struct format - matches the conversion specifiers for printf
+ * @id: specifier
+ * @f: function to pointer
  */
 
 struct format
 {
 	char *id;
-	int (*fn)(va_list, char[], int, int, int, int);
+	int (*f)();
 };
 
 /**
- * typedef struct format format_t - typedef for the struct format
+ * fmt_t - typedef of the struct format
  */
-
-typedef struct format format_t;
+typedef struct format fmt_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int handle_print(const char *format, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+int _strlen(char *s);
+int _strlenc(const char *s);
+int _string(va_list v1);
+int _char(va_list v1);
+int _37(void);
+int _HEX(va_list v1);
+int _hex(va_list vl);
+int _oct(va_list vl);
+int _unsigned(va_list args);
+int _bin(va_list vl);
+int _srev(va_list args);
+int _rot13(va_list args);
+int _int(va_list args);
+int _dec(va_list args);
+int *_strcpy(char *dest, char *src);
+int _pointer(va_list vl);
+int _hex_aux(unsigned long int num);
+int _HEX_aux(unsigned int num);
+int _exclusive_string(va_list vl);
 
-int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 #endif
