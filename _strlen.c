c901aa21fa1f4aad9f4a2d83317d08adaf1dc799
NeneNafe <nenechiwaya@gmail.com>
2023-04-24 07:47:27 -0700

    prints the length of a string

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - printes the length of a string
+ * @s: string
+ * Return: Always Success.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (c = 0; s[i] != 0; i++)
+		;
+	return (0);
+}
