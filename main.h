7916b2f8c2e5dfca2453de14575eb95bfe9c8543
NeneNafe <nenechiwaya@gmail.com>
2023-04-24 07:45:18 -0700

    header file containing all the protoypes

---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ * struct format - uses conversion specifires to print a function
+ * @id: conversion identfier
+ * @fn: a function pointer
+ */
+
+struct format
+{
+	char *id;
+	int (*fn)();
+};
+
+/**
+ * type_t - typedef for the struct format
+ */
+
+typedef strcut format type_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _strlen(char *s);
+
+#endif
